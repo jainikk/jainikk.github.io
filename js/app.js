ad3bac3bbd8fc67e669e660effd176447c6cd195
jainikk <jainikk@cybage.com>
2016-12-01 18:36:29 +0530

    module2-solution

--- js/app.js
+++ js/app.js
@@ -1,34 +1,50 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
+angular.module('ShoppingListCheckOff', [])
 
-.controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.message="";  
-
-  $scope.onCheck = function () {
-	var lunchMenu=$scope.lunchMenu;
-	var count=0;
-	if(lunchMenu==null){
-			$scope.message="Please Enter Data First"; }
-    else{
-			var lunchMenuArray=lunchMenu.split(",");
-			count=lunchMenuArray.length;
-			for(var i in lunchMenuArray){
-				if(lunchMenuArray[i]=='' || lunchMenuArray[i]==' ')
-					 count--;
-			}
-			if(count<=3 && count>0)
-				$scope.message="Enjoy!";
-			else if(count<1)
-				$scope.message="We do NOT consider an empty item, i.e., , , as an item towards to the count.";
-			else
-				$scope.message="Too much!";
-	}
-	
-	};
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService',ShoppingListCheckOffService);    
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy=this;
+  toBuy.toBuyList=ShoppingListCheckOffService.toBuyList;
+    
+    toBuy.buyItem=function(itemIndex){
+        ShoppingListCheckOffService.buyItem(itemIndex,toBuy.toBuyList);
+    }
+  
 }
-
+    
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+         var alreadyBought=this;
+  
+    alreadyBought.alreadyBoughtList=ShoppingListCheckOffService.alreadyBoughtList;
+        
+    };
+    
+    function ShoppingListCheckOffService(){
+        var service=this;
+        service.toBuyList=[{name: "cookies", quantity: 10}
+                   ,{name: "onions", quantity: 6}
+                  ,{name: "mushroom", quantity: 7}
+                  ,{name: "apples", quantity: 5}
+                  ,{name: "bananas", quantity: 3}];
+        service.alreadyBoughtList=[];
+        
+        service.buyItem=function(itemIndex,toBuyList){
+            var temp=toBuyList[itemIndex];
+            var item = {
+              name: temp.name,
+              quantity: temp.quantity
+            };
+   
+            
+       toBuyList.splice(itemIndex, 1);
+       service.alreadyBoughtList.push(item);
+            
+        }
+    }
 })();
